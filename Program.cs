102c7b22ce032df7e68b01cbb4fd5e383d769369
tulio ferreira <tuliof210@gmail.com>
2019-09-02 14:50:12 -0300

    Exemplos de Nullables

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //nullable
+            int? y = null;
+            //formas de atribuição de valor nulo para variaveis do tipo valor(que nao aceitam nulo)
+            Nullable<double> z = null;
+
+
+            //caso queira atribuit um valor para variavel sem saber se o valor é nulo:
+            int teste = y ?? 5;
+
+            Console.WriteLine(teste);
+            Console.ReadKey();
+        }
+    }
+}
