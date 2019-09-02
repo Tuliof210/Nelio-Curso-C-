2b9cd93f7ed01c998ba6018630ef73964a2eec5b
tulio ferreira <tuliof210@gmail.com>
2019-09-02 15:39:10 -0300

    Metodos com quantidade variavel de parametros (using params)

---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Soma(2, 3, 4);
+            int b = Soma(4, 10);
+
+            Console.WriteLine($"{a} e {b}");
+
+
+            Console.ReadKey();
+        }
+
+        static int Soma(params int[] x)//exemplo de metodo que recebe quantidade variavel de parametros
+        {
+            int s = 0;
+
+            foreach(int a in x)
+            {
+                s += a;
+            }
+            return s;
+        }
+    }
+}
