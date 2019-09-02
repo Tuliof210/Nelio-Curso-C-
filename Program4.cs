368e80ee608c0f7c5f60404598755330fa5df944
tulio ferreira <tuliof210@gmail.com>
2019-09-02 16:57:38 -0300

    exercicio lista para completar

---
+++ Program4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Funcionario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(string nome, int id, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void Aumento(double p)
+        {
+            Salario += Salario * (p / 100);
+        }
+
+        public override string ToString()
+        {
+            return ($"\nId:{Id}\nFuncionario: {Nome}\nSalario:{Salario}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> list = new List<Funcionario>();
+
+            string auxnome;
+            double auxsalario;
+            int auxid;
+
+            while(true)
+            {
+                Console.Write("Digite o id: ");
+                auxid = int.Parse(Console.ReadLine());
+
+                if (auxid == 0) break;
+
+                Console.Write("Digite o nome: ");
+                auxnome = Console.ReadLine();
+                Console.Write("Digite o salario: ");
+                auxsalario = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                list.Add(new Funcionario(auxnome, auxid, auxsalario));
+            }
+
+            foreach(Funcionario f in list)
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.WriteLine("\nDigite o Id do funcionario q deseja aumentar o salario: ");
+            auxid = int.Parse(Console.ReadLine());
+
+            Funcionario aux = list.Find(x => x.Id == auxid);//continua...
+
+            Console.ReadKey();
+        }
+    }
+}
