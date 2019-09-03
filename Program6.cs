e80ee9ae84ef15014df2c6dd4160995dd051cc13
tulio ferreira <tuliof210@gmail.com>
2019-09-03 15:44:40 -0300

    Ternario'

---
+++ Program6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 129.99;
+            double desconto = (price < 100.0) ? price * 0.1 : price * 0.05;
+
+            Console.WriteLine(desconto.ToString("F2"));
+            Console.ReadKey();
+        }
+    }
+}
