339c70a0e1e7baf17eb56e1f7a96aa22a11e791f
tulio ferreira <tuliof210@gmail.com>
2019-09-05 11:39:53 -0300

    Exemplo do uso de Enumeração

---
+++ Enum/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Curso.Entities.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nHora: {Moment}\nStatus: {Status}";
+        }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Curso.Entities;
+using Curso.Entities.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order pedido = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(pedido);
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus),"Shipped");//String to Enum
+            Console.WriteLine(os);
+
+            Console.ReadKey();
+        }
+    }
+}
