9a9db69434a98f77842ddcba32e1ce4a0f16e3fd
Tulio Ferreira <tuliof210@gmail.com>
2019-09-09 14:41:06 -0300

    Tratamento de Exceçooes e Throw

---
+++ Exception_Throw/Entities/CalculaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Curso.Exception;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Entities
+{
+    class CalculaData
+    {
+        public DateTime Data1 { get; set; }
+        public DateTime Data2 { get; set; }
+
+        public CalculaData(DateTime data1, DateTime data2)
+        {
+            Data1 = data1;
+            Data2 = data2;
+        }
+
+        public double Subtract()
+        {
+            if (Data2 > Data1) throw new DomainException("A Segunda data nao pode ser maior");
+
+            TimeSpan ts = Data1.Subtract(Data2);
+            return ts.TotalMinutes;
+        }
+    }
+}
---
+++ Exception_Throw/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curso.Entities;
+using Curso.Exception;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = new DateTime(2020, 5, 22);
+            DateTime dt2 = new DateTime(2019, 5, 22);
+
+            try
+            {
+                CalculaData cd = new CalculaData(dt1, dt2);
+
+                Console.WriteLine(cd.Subtract());
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
