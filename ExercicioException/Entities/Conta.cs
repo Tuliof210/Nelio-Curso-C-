78219879ac332aa269c519dac878f3742bd20c51
Tulio Ferreira <tuliof210@gmail.com>
2019-09-09 15:01:58 -0300

    exercicio exception

---
+++ ExercicioException/Entities/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curso.Exception;
+
+namespace Curso.Entities
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+        public double LimiteSaque { get; private set; }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+            LimiteSaque = saldo * 0.5;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor <= 0) throw new DomainException("Não é possível realizar depositos de valores inferiores a $1");
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= 0) throw new DomainException("Não é possível realizar saques de valores inferiores a $1");
+            if (valor > LimiteSaque) throw new DomainException("O valor informado é maior que seu limite atual de saque");
+            if (valor > Saldo) throw new DomainException("Saldo insuficiente");
+            Saldo -= valor;
+        }
+    }
+}
---
+++ ExercicioException/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curso.Entities;
+using Curso.Exception;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular;
+            Random r = new Random(1000);
+            double saldo;
+
+            Conta c1;
+
+            try
+            {
+                Console.Write("Informe o titular: ");
+                titular = Console.ReadLine();
+                Console.Write("Informe o saldo inicial: ");
+                saldo = double.Parse(Console.ReadLine());
+
+                c1 = new Conta(r.Next(), titular, saldo);
+
+                c1.Depositar(2080);
+                c1.Sacar(-2080);
+
+                Console.WriteLine(c1.Saldo);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
