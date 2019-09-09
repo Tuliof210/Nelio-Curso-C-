ce0e3f2b54132d6b858c78dbd19de63c09cfbc9b
Tulio Ferreira <tuliof210@gmail.com>
2019-09-09 15:40:56 -0300

    Classe posiçao

--- Projeto01_Xadrez/Projeto01_Xadrez/Program.cs
+++ Projeto01_Xadrez/Projeto01_Xadrez/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using Tabuleiro;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projeto01_Xadrez
+namespace Xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Position p = new Position(3, 4);
 
+            Console.WriteLine(p);
             Console.ReadKey();
         }
     }
