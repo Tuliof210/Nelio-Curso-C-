0cfc530a2959e24fa0fdbaf10f7ff9f6fc76b4cf
Tulio Ferreira <tuliof210@gmail.com>
2019-09-11 16:39:30 -0300

    Exercicio usando interfaces/Inversao de controle/Injeção de Dependencia V2

--- ExercicioInterfaces/Interfaces/IPaymentServices.cs
+++ ExercicioInterfaces/Interfaces/IPaymentServices.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Curso.Entities;
 
 namespace Curso.Interfaces
--- ExercicioInterfaces/Program.cs
+++ ExercicioInterfaces/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Curso.Entities;
 using Curso.Exceptions;
-using Curso.Interfaces;
 
 namespace Curso
 {
